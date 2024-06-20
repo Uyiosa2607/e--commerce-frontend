7126a2bcf342e1b05b7799402e8cd1a34e01a531
osas4real <aghahowaosamudiamen03@gmail.com>
2024-06-20 13:34:17 +0100

    designed navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
-import Navbar from "@/components/navbar";
+/* eslint-disable @next/next/no-img-element */
+import Navbar from "@/components/items/navbar";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -23,203 +24,27 @@ export default function Home() {
   return (
     <main className="w-full">
       <Navbar />
-      <div className="w-[90%] px-2 mx-auto h-[500px] bg-pink-300 rounded-2xl my-1"></div>
-      <section className="flex  flex-col lg:flex-row w-[80%] mx-auto">
-        <div className="flex flex-col items-center justify-center gap-3">
-          <h3>deals of the month</h3>
-          <p className="w-[80%]">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi
-            sit molestias vero repudiandae ipsam nesciunt eveniet, voluptate
-            sapiente molestiae explicabo? Consectetur dolores pariatur quos
-            blanditiis beatae, eum fugiat tempore. Vel!
-          </p>
-          <Button>buy now</Button>
-          <div className="timer">
-            <span>TIMER GOES HERE</span>
+      <section className="flex h-[600px]">
+        <img
+          className="object-cover flex-1 h-auto w-full"
+          src="/landing-main-image.png"
+          alt="images of spoon and seeds on a table"
+        />
+        <div className="flex-1 flex items-center justify-center">
+          <div className="pl-[100px] flex justify-center flex-col gap-6">
+            <h3 className="text-[25px] font-bold">
+              Everyday is unique just like our tea
+            </h3>
+            <p className="w-[60%] text-[14px]">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+              vitae incidunt perspiciatis consequatur tempora, eius labore Lorem
+              ipsum dolor sit amet consectetur adipisicing elit. Beatae tempore
+              recusandae numquam tempore totam odio obcaecati dolorem eum est.
+            </p>
+            <button className="outline-none w-fit text-[14px] py-[10px] px-[40px] uppercase bg-black text-white">
+              Browse teas
+            </button>
           </div>
-        </div>
-        <div>
-          <Carousel className="w-[20%] mx-auto">
-            <CarouselContent>
-              {Array.from({ length: 6 }).map((_, index) => (
-                <CarouselItem key={index}>
-                  <div className="p-1">
-                    <Card>
-                      <CardContent className="flex w-[40px] flex-col lg:flex-row h-[40px] items-center justify-center p-2">
-                        <span className="text-4xl font-semibold">
-                          {index + 1}
-                        </span>
-                      </CardContent>
-                    </Card>
-                  </div>
-                </CarouselItem>
-              ))}
-            </CarouselContent>
-            <CarouselPrevious />
-            <CarouselNext />
-          </Carousel>
-        </div>
-      </section>
-      <section>
-        <h2>new arrivals</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Et fuga ab
-          sunt laborum amet.
-        </p>
-        <div>
-          <Tabs defaultValue="account" className="w-full">
-            <TabsList className="flex justify-between items-center lg:gap-[200px]">
-              <TabsTrigger value="account">men</TabsTrigger>
-              <TabsTrigger value="children">children</TabsTrigger>
-              <TabsTrigger value="women">women</TabsTrigger>
-              <TabsTrigger value="adult">adult</TabsTrigger>
-              <TabsTrigger value="fashion">men fashion</TabsTrigger>
-            </TabsList>
-            <TabsContent value="account">
-              <Card>
-                <CardHeader>
-                  <CardTitle>Account</CardTitle>
-                  <CardDescription>
-                    Make changes to your account here. Click save when youre
-                    done.
-                  </CardDescription>
-                </CardHeader>
-                <CardContent className="space-y-2">
-                  <div className="space-y-1">
-                    <Label htmlFor="name">Name</Label>
-                    <Input id="name" defaultValue="Pedro Duarte" />
-                  </div>
-                  <div className="space-y-1">
-                    <Label htmlFor="username">Username</Label>
-                    <Input id="username" defaultValue="@peduarte" />
-                  </div>
-                </CardContent>
-                <CardFooter>
-                  <Button>Save changes</Button>
-                </CardFooter>
-              </Card>
-            </TabsContent>
-            <TabsContent value="women">
-              <Card>
-                <CardHeader>
-                  <CardTitle>Password</CardTitle>
-                  <CardDescription>
-                    Change your password here. After saving, you'll be logged
-                    out.
-                  </CardDescription>
-                </CardHeader>
-                <CardContent className="space-y-2">
-                  <div className="space-y-1">
-                    <Label htmlFor="current">Current password</Label>
-                    <Input id="current" type="password" />
-                  </div>
-                  <div className="space-y-1">
-                    <Label htmlFor="new">New password</Label>
-                    <Input id="new" type="password" />
-                  </div>
-                </CardContent>
-                <CardFooter>
-                  <Button>Save password</Button>
-                </CardFooter>
-              </Card>
-            </TabsContent>
-            <TabsContent value="men">
-              <Card>
-                <CardHeader>
-                  <CardTitle>Password</CardTitle>
-                  <CardDescription>
-                    Change your password here. After saving, you'll be logged
-                    out.
-                  </CardDescription>
-                </CardHeader>
-                <CardContent className="space-y-2">
-                  <div className="space-y-1">
-                    <Label htmlFor="current">Current password</Label>
-                    <Input id="current" type="password" />
-                  </div>
-                  <div className="space-y-1">
-                    <Label htmlFor="new">New password</Label>
-                    <Input id="new" type="password" />
-                  </div>
-                </CardContent>
-                <CardFooter>
-                  <Button>Save password</Button>
-                </CardFooter>
-              </Card>
-            </TabsContent>
-            <TabsContent value="fashion">
-              <Card>
-                <CardHeader>
-                  <CardTitle>Password</CardTitle>
-                  <CardDescription>
-                    Change your password here. After saving, you'll be logged
-                    out.
-                  </CardDescription>
-                </CardHeader>
-                <CardContent className="space-y-5">
-                  <div className="space-y-1">
-                    <Label htmlFor="current">Current password</Label>
-                    <Input id="current" type="password" />
-                  </div>
-                  <div className="space-y-1">
-                    <Label htmlFor="new">New password</Label>
-                    <Input id="new" type="password" />
-                  </div>
-                </CardContent>
-                <CardFooter>
-                  <Button>Save password</Button>
-                </CardFooter>
-              </Card>
-            </TabsContent>
-            <TabsContent value="children">
-              <Card>
-                <CardHeader>
-                  <CardTitle>Password</CardTitle>
-                  <CardDescription>
-                    Change your password here. After saving, you'll be logged
-                    out.
-                  </CardDescription>
-                </CardHeader>
-                <CardContent className="space-y-2">
-                  <div className="space-y-1">
-                    <Label htmlFor="current">Current password</Label>
-                    <Input id="current" type="password" />
-                  </div>
-                  <div className="space-y-1">
-                    <Label htmlFor="new">New password</Label>
-                    <Input id="new" type="password" />
-                  </div>
-                </CardContent>
-                <CardFooter>
-                  <Button>Save password</Button>
-                </CardFooter>
-              </Card>
-            </TabsContent>
-            <TabsContent value="adult">
-              <Card>
-                <CardHeader>
-                  <CardTitle>Password</CardTitle>
-                  <CardDescription>
-                    Change your password here. After saving, you'll be logged
-                    out.
-                  </CardDescription>
-                </CardHeader>
-                <CardContent className="space-y-2">
-                  <div className="space-y-1">
-                    <Label htmlFor="current">Current password</Label>
-                    <Input id="current" type="password" />
-                  </div>
-                  <div className="space-y-1">
-                    <Label htmlFor="new">New password</Label>
-                    <Input id="new" type="password" />
-                  </div>
-                </CardContent>
-                <CardFooter>
-                  <Button>Save password</Button>
-                </CardFooter>
-              </Card>
-            </TabsContent>
-          </Tabs>
         </div>
       </section>
     </main>
---
+++ components/items/navbar.tsx
@@ -0,0 +1,25 @@
+/* eslint-disable @next/next/no-img-element */
+
+export default function Navbar() {
+  return (
+    <header className="py-[30px] px-1">
+      <div className="wrapper flex justify-between items-center">
+        <div className="left flex gap-1 items center">
+          <img src="/logo.png" alt="logo" />
+          <span className="pt-2 text-[20px] font-bold">brand name</span>
+        </div>
+        <div className="middle text-[14px] font-medium uppercase flex gap-[30px] items-center">
+          <p>tea collection</p>
+          <p>accesories</p>
+          <p>blog</p>
+          <p>contact us</p>
+        </div>
+        <div className="right flex gap-[20px] items-center">
+          <img src="/search.png" alt="search" />
+          <img src="/person.png" alt="person" />
+          <img src="/local_mall.png" alt="cart" />
+        </div>
+      </div>
+    </header>
+  );
+}
