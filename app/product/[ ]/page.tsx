52cca35fbe02fedce0c04224e186ef4b8c571024
osas4real <aghahowaosamudiamen03@gmail.com>
2024-06-22 14:12:05 +0100

    added product page

---
+++ app/product/[ ]/page.tsx
@@ -0,0 +1,12 @@
+import Footer from "@/components/items/footer";
+import Navbar from "@/components/items/navbar";
+
+export default function Product() {
+  return (
+    <main>
+      <Navbar />
+      <span className="capitalize">this is product page</span>
+      <Footer />
+    </main>
+  );
+}
