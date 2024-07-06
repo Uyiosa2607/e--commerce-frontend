29b58be571afd9dec9514b2916dcd03606698b5e
osas4real <aghahowaosamudiamen03@gmail.com>
2024-07-06 14:27:35 +0100

    added mobile navbar

--- components/items/navbar.tsx
+++ components/items/navbar.tsx
@@ -1,15 +1,25 @@
 /* eslint-disable @next/next/no-img-element */
+"use client";
+
+import { useState } from "react";
+import Link from "next/link";
 import {
   MdOutlinePersonOutline,
   MdOutlineSearch,
   MdOutlineShoppingBag,
 } from "react-icons/md";
 import { GiHamburgerMenu } from "react-icons/gi";
-import Link from "next/link";
+import { AiOutlineClose } from "react-icons/ai";
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <header className="py-[10px] lg:py-[30px] left-0 top-0 px-1 sticky w-full bg-white">
+    <header className="py-[4px] lg:py-[30px] left-0 top-0 px-1 sticky w-full bg-white z-50">
       <div className="container p-[10px] wrapper flex justify-between items-center">
         <Link href="/">
           <div className="left flex gap-1 items center">
@@ -17,8 +27,8 @@ export default function Navbar() {
             <span className="pt-1 text-[20px] font-bold">brand name</span>
           </div>
         </Link>
-        <div className="hidden">
-          <div className="middle text-[14px] font-medium uppercase flex gap-[30px] items-center">
+        <div className="">
+          <div className="hidden middle text-[14px] font-medium uppercase lg:flex gap-[30px] items-center">
             <Link href="/collection">
               <p>tea collections</p>
             </Link>
@@ -32,24 +42,43 @@ export default function Navbar() {
               <p>contact us</p>
             </Link>
           </div>
-          <div className="text-14px hidden text-[14px]">
-            <div>
-              <MdOutlineSearch className="cursor-pointer" />
-              <input placeholder="SEARCH PRODUCTS" type="text" />
+          <div
+            className={`transition-transform duration-300 ease-in-out ${
+              isOpen ? "transform translate-x-0" : "transform translate-x-full"
+            }  lg:hidden pl-[12px] flex flex-col gap-3 text-[14px] absolute top-[10vh] pt-2 right-0 h-screen bg-white w-[47%]`}
+          >
+            <div className="flex border-[1px] border-black p-[4px] items-center gap-1 mb-2">
+              <MdOutlineSearch className="cursor-pointer text-[22px]" />
+              <input
+                className="text-[12px]"
+                placeholder="SEARCH PRODUCTS"
+                type="text"
+              />
             </div>
-            <div>
-              <MdOutlinePersonOutline className="cursor-pointer" />
-              <div>
-                <p>user account</p>
-                <p>we know you as a guest user</p>
+            <div className="flex items-center gap-1">
+              <MdOutlinePersonOutline className="cursor-pointer  text-[25px]" />
+              <div className="m-0 p-0">
+                <p className="uppercase text-[10px] font-[600]">user account</p>
+                <p className="text-[11px] text-gray-400">
+                  we know you as a guest user
+                </p>
               </div>
             </div>
-            <div>
-              <MdOutlineShoppingBag className="cursor-pointer" />
+            <div className="flex items-center gap-1">
+              <MdOutlineShoppingBag className="cursor-pointer text-[25px]" />
               <div>
-                <p>your bag</p>
-                <p>items has been added</p>
+                <p className="uppercase text-[10px] font-[600]">your bag</p>
+                <p className="text-[11px]  text-gray-400">
+                  items has been added
+                </p>
               </div>
+            </div>
+            <div className="w-[80%] mx-auto my-[10px] bg-gray-300 h-[1px]"></div>
+            <div className="flex flex-col gap-3 text-[12px] font-[500] pl-[12px]">
+              <p className="uppercase">tea collections</p>
+              <p className="uppercase">accesories</p>
+              <p className="uppercase">blog</p>
+              <p className="uppercase">contact us</p>
             </div>
           </div>
         </div>
@@ -57,14 +86,27 @@ export default function Navbar() {
         <div className="hidden text-[24px] lg:flex gap-[20px] items-center">
           <MdOutlineSearch className="cursor-pointer" />
           <Link href="/account">
-            {" "}
-            <MdOutlinePersonOutline className="cursor-pointer" />
+            <p>
+              <MdOutlinePersonOutline className="cursor-pointer" />
+            </p>
           </Link>
           <Link href="/cart">
-            <MdOutlineShoppingBag className="cursor-pointer" />
+            <p>
+              <MdOutlineShoppingBag className="cursor-pointer" />
+            </p>
           </Link>
         </div>
-        <GiHamburgerMenu className="lg:hidden text-[24px]" />
+        {isOpen ? (
+          <AiOutlineClose
+            onClick={handleNavbar}
+            className="lg:hidden text-[24px]"
+          />
+        ) : (
+          <GiHamburgerMenu
+            onClick={handleNavbar}
+            className="lg:hidden text-[24px]"
+          />
+        )}
       </div>
     </header>
   );
