ae54ab5b164fbee1c2ffc2125f1d90bd8f0eefa4
osas4real <aghahowaosamudiamen03@gmail.com>
2024-07-06 14:40:11 +0100

    fixed overflow issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html className="overflow-x-hidden" lang="en">
       <body>
         <Navbar />
         {children}
